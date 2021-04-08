d6fd9bc3430ca94bfa7ee5de0db725bfdb3b5c8c
radrow <radrowicki@gmail.com>
2021-04-08 16:44:12 +0200

    add kurwamac refiner

--- test/aeso_type_refinement_tests.erl
+++ test/aeso_type_refinement_tests.erl
@@ -19,31 +19,10 @@ run_test(Test) ->
       end || {Label, Fun} <- ?MODULE:TestFun() ],
     ok.
 
-%%  Very simply test compile the given contracts. Only basic checks
-%%  are made on the output, just that it is a binary which indicates
-%%  that the compilation worked.
-%% template_gen_test_() ->
-%%     [ {"Testing liquid template generation of " ++ ContractName,
-%%        fun() ->
-%%            case gen_templates(ContractName) of
-%%                {ok, AST} ->
-%%                    aeso_ast:pp(AST), error(xd);
-%%                {error, ErrBin} ->
-%%                    io:format("\n~s", [ErrBin]),
-%%                    error(ErrBin)
-%%            end
-%%        end} || ContractName <- compilable_contracts()].
-
-%% gen_templates(Name) ->
-%%     ContractString = aeso_test_utils:read_contract(Name),
-%%     Ast = aeso_parser:string(ContractString, sets:new(), []),
-%%     {_, _, TAst} = aeso_ast_infer_types:infer(Ast, [return_env]),
-%%     R = aeso_ast_refine_types:decorate_base_types(TAst),
-%%     {ok, R}.
-
 constraints_gen_test_() ->
-        [ {"Testing liquid template generation of " ++ ContractName,
-           fun() ->
+    aeso_kurwamac:init_refiner(),
+    [ {"Testing liquid template generation of " ++ ContractName,
+       fun() ->
                case gen_constraints(ContractName) of
                    {ok, AST} ->
                        io:format("~p", [AST]), error(xd);
@@ -57,7 +36,7 @@ gen_constraints(Name) ->
     Ast = aeso_parser:string(ContractString, sets:new(), []),
     {_, _, TAst} = aeso_ast_infer_types:infer(Ast, [return_env]),
     [ begin
-          {T, Cs} = aeso_ast_refine_types:constr_letfun(aeso_ast_refine_types:init_env(), Decl, []),
+          {T, Cs} = aeso_kurwamac:constr_letfun(aeso_kurwamac:init_env(), Decl, []),
           io:format("AA ~p", [T]),
           io:format("\n*** INFERRED\n\n~s\n\n",
                     [prettypr:format(aeso_pretty:dep_type(T))
