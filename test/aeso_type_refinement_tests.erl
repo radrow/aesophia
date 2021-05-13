2a0d2f720c2af96ffc9fd56a0d180e3243477431
radrow <radrowicki@gmail.com>
2021-05-13 10:47:47 +0200

    .

--- test/aeso_type_refinement_tests.erl
+++ test/aeso_type_refinement_tests.erl
@@ -23,14 +23,14 @@ solver_test_() ->
     aeso_smt:start_z3(),
     [ { "SMT solver test"
       , fun() ->
-              ?assert(aeso_kurwamac:impl_holds(
+              ?assert(aeso_ast_refine_types:impl_holds(
                         maps:from_list([{"x", {id, [], "int"}}]), [], [{app, [], {'==', []}, [{id, [], "x"}, {id, [], "x"}]}]))
       end
       }
     ].
 
 constraints_gen_test_() ->
-    aeso_kurwamac:init_refiner(),
+    aeso_ast_refine_types:init_refiner(),
     [ {"Testing liquid template generation of " ++ ContractName,
        fun() ->
                try gen_constraints(ContractName) of
@@ -48,27 +48,33 @@ gen_constraints(Name) ->
     Ast = aeso_parser:string(ContractString, sets:new(), []),
     {_, _, TAst} = aeso_ast_infer_types:infer(Ast, [return_env]),
     [ begin
-          {Con1, E, Cs} = aeso_kurwamac:constr_con(Con),
-          Cs1 = aeso_kurwamac:simplify(Cs),
-          Cs2 = aeso_kurwamac:group_subtypes(Cs1),
-          Assg = aeso_kurwamac:solve(E, Cs2),
-          [ io:format("~s : ~s\n", [Name, aeso_pretty:pp(dep_type, aeso_kurwamac:apply_assg(Assg, T))])
+          {{contract, _, _, Defs}, E, Cs} = aeso_ast_refine_types:constr_con(Con),
+          Cs1 = aeso_ast_refine_types:simplify(Cs),
+          Cs2 = aeso_ast_refine_types:group_subtypes(Cs1),
+          %% Cs2 = Cs1,
+          Assg = aeso_ast_refine_types:solve(E, Cs2),
+          io:format("\n\n**** EXTERNAL:\n"),
+          [ io:format("~s : ~s\n", [Name, aeso_pretty:pp(dep_type, aeso_ast_refine_types:apply_assg(Assg, T))])
            || {Name, T} <- maps:to_list(element(2, E))
+          ],
+          io:format("\n\n**** INTERNAL:\n"),
+          [ io:format("_ : ~s\n", [aeso_pretty:pp(dep_type, aeso_ast_refine_types:apply_assg(Assg, T))])
+            || T <- Defs
           ]
       end
      || Con <- TAst
     ],
     %% [ begin
-    %%       {T, Cs} = aeso_kurwamac:constr_letfun(aeso_kurwamac:init_type_env(), Decl, []),
+    %%       {T, Cs} = aeso_ast_refine_types:constr_letfun(aeso_ast_refine_types:init_type_env(), Decl, []),
     %%       io:format("\n*** INFERRED\n\n~s\n\n",
     %%                 [prettypr:format(aeso_pretty:dep_type(T))
     %%                 ]),
-    %%       Cs1 = aeso_kurwamac:simplify(Cs),
-    %%       Cs2 = aeso_kurwamac:group_subtypes(Cs1),
+    %%       Cs1 = aeso_ast_refine_types:simplify(Cs),
+    %%       Cs2 = aeso_ast_refine_types:group_subtypes(Cs1),
     %%       io:format("**** CONSTRAINTS ****\n\n"),
     %%       [io:format("~s\n\n", [prettypr:format(aeso_pretty:constr(C))]) || C <- Cs],
-    %%       Assg = aeso_kurwamac:solve(Cs2),
-    %%       T1 = aeso_kurwamac:apply_assg(Assg, T),
+    %%       Assg = aeso_ast_refine_types:solve(Cs2),
+    %%       T1 = aeso_ast_refine_types:apply_assg(Assg, T),
     %%       io:format("**** SOLVED ****\n\n~s\n", [aeso_pretty:pp(dep_type, T1)])
     %%   end
     %%   || {contract, _, _, Decls} <- TAst,
