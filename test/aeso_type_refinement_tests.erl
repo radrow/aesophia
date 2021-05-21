0f49f6a2ec50ab53aa71287feb77b97d2d213057
radrow <radrowicki@gmail.com>
2021-05-21 12:57:45 +0200

    Fix return fresh

--- test/aeso_type_refinement_tests.erl
+++ test/aeso_type_refinement_tests.erl
@@ -34,7 +34,6 @@ hagia_test_() ->
 smt_solver_test_group() ->
     [ { "x == x"
       , fun() ->
-                io:format("aAAA"),
                 ?assert(aeso_ast_refine_types:impl_holds(
                           aeso_ast_refine_types:bind_var(
                             {id, [], "x"}, {id, [], "int"},
@@ -46,33 +45,56 @@ smt_solver_test_group() ->
     ].
 
 refiner_test_group() ->
-    [ {"Testing liquid template generation of " ++ ContractName,
+    [ {"Testing type refinement of " ++ ContractName,
        fun() ->
-               try run_refine(ContractName) of
-                   {ok, AST} ->
-                       io:format("~s", [aeso_pretty:pp(decls, AST)]), error(success);
-                   {error, {contradict, Assump, Promise}} ->
+               try {run_refine("hagia/" ++ ContractName), Expect} of
+                   {{ok, AST}, {success, Assertions}} ->
+                       io:format("AST:\n~s\n\n", [aeso_pretty:pp(decls, AST)]),
+                       check_ast_refinement(AST, Assertions);
+                   {{error, {contradict, Assump, Promise}}, {contradict, ExAssump, ExPromise}} ->
+                       ?assertEqual(ExAssump, Assump),
+                       ?assertEqual(ExPromise, Promise);
+                   {{error, {contradict, Assump, Promise}}, _} ->
                        io:format("Could not prove the promise\n  ~s\n"
-                                    "from the assumption\n  ~s\n",
-                                 [aeso_pretty:pp(predicate, Promise), aeso_pretty:pp(predicate, Assump)]
+                                 "from the assumption\n  ~s\n",
+                                 [aeso_prettyy:pp(predicate, Promise), aeso_pretty:pp(predicate, Assump)]
                                 ),
                        error(contradict);
-                   {error, ErrBin} ->
+                   {{error, ErrBin}, _} ->
                        io:format("\n~s", [ErrBin]),
                        error(ErrBin)
-               catch E:T:S -> io:format("\n\n\n***** CHUJ 8===>\n~p: ~p\nstack:\n~p\n", [E, T, S]), error(T)
+               catch E:T:S -> io:format("Caught:\n~p: ~p\nstack:\n~p\n", [E, T, S]), error(T)
                end
-       end} || ContractName <- compilable_contracts()].
+       end} || {ContractName, Expect} <- compilable_contracts()].
 
 
 run_refine(Name) ->
-    io:format("no elo\n\n"),
     ContractString = aeso_test_utils:read_contract(Name),
     Ast = aeso_parser:string(ContractString, sets:new(), []),
     {_, _, TAst} = aeso_ast_infer_types:infer(Ast, [return_env]),
     RAst = aeso_ast_refine_types:refine_ast(TAst),
     RAst.
 
+check_ast_refinement(AST, Assertions) ->
+    [ case maps:get({Name, FName}, Assertions, unchecked) of
+          unchecked -> ok;
+          ExType -> check_type(ExType, Type)
+      end
+     || {_, _, {con, _, Name}, Defs} <- AST,
+        {fun_decl, _, {id, _, FName}, Type} <- Defs
+    ].
+
+check_type(ExQuals, {dep_fun_t, _, _, _, {refined_t, _, _, Quals}}) ->
+    ?assertEqual(ExQuals, aeso_pretty:pp(predicate, Quals)).
+
 %% compilable_contracts() -> [ContractName].
 %%  The currently compilable contracts.
-compilable_contracts() -> ["hagia"].
+compilable_contracts() ->
+    [{"max",
+      {success,
+       #{{"C", "max"} => "$nu >= b && $nu >= a"
+       , {"C", "trim"} => "$nu > -1 && $nu >= x"
+       }
+      }
+     }
+    ].
