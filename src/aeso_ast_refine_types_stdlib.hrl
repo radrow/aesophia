b9fc78b011727762cde243285e87bc39637fc87c
radrow <radrowicki@gmail.com>
2021-06-24 13:28:59 +0200

    List tail

--- src/aeso_ast_refine_types_stdlib.hrl
+++ src/aeso_ast_refine_types_stdlib.hrl
@@ -29,6 +29,7 @@ constr_expr(Env, {app, Ann, {typed, _, {qid, _, [NS, Fun]}, {fun_t, _, [], ArgsT
                }
            end
           )
+
     ?CONSTR("List", "first", [L],
            begin
                {{dep_list_t, _, _, ElemT, _}, S1} = constr_expr(Env, L, S0),
@@ -50,6 +51,7 @@ constr_expr(Env, {app, Ann, {typed, _, {qid, _, [NS, Fun]}, {fun_t, _, [], ArgsT
                }
            end
            )
+
     ?CONSTR("List", "tail", [L],
            begin
                {{dep_list_t, _, _, ElemT, _}, S1} = constr_expr(Env, L, S0),
@@ -73,6 +75,7 @@ constr_expr(Env, {app, Ann, {typed, _, {qid, _, [NS, Fun]}, {fun_t, _, [], ArgsT
                }
            end
            )
+
     ?CONSTR("List", "last", [L],
            begin
                {{dep_list_t, _, _, ElemT, _}, S1} = constr_expr(Env, L, S0),
@@ -94,4 +97,24 @@ constr_expr(Env, {app, Ann, {typed, _, {qid, _, [NS, Fun]}, {fun_t, _, [], ArgsT
                }
            end
            )
+
+    ?CONSTR("List", "find_indices", [P, L], %% TODO: len == 0 if no way to fulfill
+           begin
+               {_, S1} = constr_expr(Env, P, S0),
+               {_, S2} = constr_expr(Env, L, S1),
+               ExprT = {dep_list_t, _, _, ElemT, _} = fresh_liquid(Env, "find_indices", RetT),
+               LId = fresh_id(Ann, "find_indices_l"),
+               { ExprT
+               , [ {well_formed, constr_id(), Env, ExprT}
+                 , {subtype, constr_id(), Ann,
+                    {dep_list_t, Ann, LId, ElemT, [?op(Ann, LId, '=<', L)]},
+                    ExprT
+                   }
+                 , {subtype, constr_id(), Env, ?d_nonneg_int(Ann), ElemT}
+                 | S2
+                 ]
+               }
+           end
+           )
+
   ).
